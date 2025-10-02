0ff70bb862c251520047ae75f9a62f1c662675d2
hglass <gjdbfl45@naver.com>
2025-10-02 13:51:45 +0900

    기준 영역 및 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
+import Main from "./screens/Main";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <p>모바일 청첩장</p>
-      <p>모바일 청첩장</p>
-      <p>모바일 청첩장</p>
+      <Main />
     </div>
   );
 }
-
-export default App;
